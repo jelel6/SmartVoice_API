f64b823c8d5833247365fee33022cad258261be1
jelel6 <abduljelelahmed@gmail.com>
2020-02-17 11:02:30 -0800

    updating database

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const saveResponse = require('./controllers/saveResponse');
 const db = knex({
     client: 'pg',    
     connection: {
-    host : process.env.DATABASE_URL,
+    connectionString : process.env.DATABASE_URL,
     ssl: true,
   },
 });
