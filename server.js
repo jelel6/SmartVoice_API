87f6b5fff1e226e2ff3fa3777e2f0a02596ce9b2
jelel6 <abduljelelahmed@gmail.com>
2020-02-17 10:47:10 -0800

    updating database

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const saveResponse = require('./controllers/saveResponse');
 const db = knex({
     client: 'pg',    
     connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-octagonal-03766',
     user : 'postgres',
     password : 'jelel',
     database : 'SmartVoice'
